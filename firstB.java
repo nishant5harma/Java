b6abd4c0d3d402507547be63457e7061a08bb955
Nishant <shiva5h4rm4@gmail.com>
2023-04-09 23:33:07 +0530

    First File

--- firstB.java
+++ firstB.java
@@ -1,7 +1,8 @@
 public class firstB {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Learning Java");
+         
     }
     
 }
